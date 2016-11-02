94c436be07a16a14b97b8cf363c58afcb4c5d65f
薛丞宏 <ihcaoe@gmail.com>
2016-11-02 10:23:26 +0800

    規則會振動矣

---
+++ src/App/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import 後端 from './後端';
+
+import './App.css';
+
+import Transmit from 'react-transmit';
+import { Promise } from 'bluebird';
+import { Link } from 'react-router';
+var superagent = require('superagent-promise')(require('superagent'), Promise);
+import Debug from 'debug';
+var debug = Debug('itaigi:App');
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+    <div className='app background'>
+      <h1 className='ui  blue header'>臺語語音辨識</h1>
+        { React.cloneElement(
+          this.props.children,
+          {
+            後端網址: 後端.網址(),
+          }
+        )}
+    </div>
+    );
+  }
+}
+
+App.propTypes = {
+  history: React.PropTypes.object,
+  children: React.PropTypes.object,
+};
+
---
+++ src/頁/錄.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Debug from 'debug';
+var debug = Debug('itaigi:App');
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+    <div className='app background'>
+    XX
+    </div>
+    );
+  }
+}
