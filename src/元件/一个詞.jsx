954175bcfcfe5766efab3755d328722f66c3c603
fafoy <ihcaoe@gmail.com>
2016-11-30 11:04:20 +0800

    漢字臺羅用ruby

---
+++ src/元件/一个詞.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Debug from 'debug';
+
+var debug = Debug('itaigi:一个詞');
+
+export default class 一个詞 extends React.Component {
+
+  放送() {
+    let 音樂 = this.refs.音樂;
+    音樂.play();
+  }
+
+  render() {
+    let { 分詞, 漢字, 臺羅 } = this.props;
+
+    return (
+      <span >
+      <ruby onClick={this.放送.bind(this)}>
+        {漢字}
+        <rt> {臺羅} </rt>
+      </ruby>
+        
+      <audio ref="音樂">
+        <source type='audio/wav'
+          src={encodeURI(
+            'http://voice.itaigi.tw/語音合成?查詢腔口=閩南語&查詢語句=' + 分詞
+          )}/>
+      </audio>
+        
+      </span>
+    );
+  }
+}
--- src/元件/漢字臺羅.jsx
+++ src/元件/漢字臺羅.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Debug from 'debug';
-import 錄音控制 from '../元件/錄音控制';
-import 音檔表 from '../元件/音檔表';
+import 一个詞 from '../元件/一个詞';
 
 var debug = Debug('itaigi:漢字臺羅');
 
@@ -17,24 +16,35 @@ export default class 漢字臺羅 extends React.Component {
     if (nextProps.資料 === this.props.資料) return;
   }
 
+  ss(分詞) {
+    let 音樂 = this.refs.音樂;
+    音樂.src = encodeURI('http://voice.itaigi.tw/語音合成?查詢腔口=閩南語&查詢語句=' + 分詞);
+    音樂.load();
+    音樂.play();
+  }
+
   render() {
-    let { 編號, 漢字, 臺羅 } = this.props.資料;
+    let { 編號 } = this.props.資料;
     let { 分詞, 綜合標音 } = this.props.漢字音標對齊;
-    let 分詞陣列=[],漢字陣列=[], 臺羅陣列=[]
-    綜合標音.map(function(標音){
-      分詞陣列.concat(標音.分詞.split(' '))
-      漢字陣列.concat(標音.漢字.split(' '))
-      臺羅陣列.concat(標音.臺羅.split(' '))
-    // 漢字, 臺羅.concat
-    })
-    //
+    let 分詞陣列 = [];
+    let 漢字陣列 = [];
+    let 臺羅陣列 = [];
+    綜合標音.map(function (標音) {
+      分詞陣列 = 分詞陣列.concat(標音.分詞.split(' '));
+      漢字陣列 = 漢字陣列.concat(標音.漢字.split(' '));
+      臺羅陣列 = 臺羅陣列.concat(標音.臺羅閏號調.split(' '));
+    });
+
+    let 詞 = 分詞陣列.map((分詞, i)=>(
+      <一个詞 key={i}
+        分詞={分詞} 漢字={漢字陣列[i]} 臺羅={臺羅陣列[i]} />
+    ));
+
     return (
-    <div>
-      {編號}<br/>
-      {漢字}<br/>
-      {臺羅}<br/>
-      {分詞}<br/>
-    </div>
+      <div>
+        {編號}<br/>
+        {詞}<br/>
+      </div>
     );
   }
 }
